d420dfd4c08b69126bbfc3f73561a4c7db21c37b
malluneha-debug <reddymalluneha@gmail.com>
2026-07-10 14:20:59 +0530

    Create User model with schema and password methods

---
+++ Document/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    address: { type: String },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ['User', 'Admin', 'Organizer'],
+      default: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
