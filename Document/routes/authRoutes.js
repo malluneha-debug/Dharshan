eaedade449c90aba913e5942db06afd24042eb4f
malluneha-debug <reddymalluneha@gmail.com>
2026-07-10 14:21:48 +0530

    Add authentication routes for register, login, and logout

---
+++ Document/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {
+  registerUser,
+  authUser,
+  logoutUser,
+} = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', authUser);
+router.post('/logout', logoutUser);
+
+module.exports = router;
